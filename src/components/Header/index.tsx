4a30c164305ffd4ed3e3959b8b1c7e34df399c3c
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-03-29 09:36:00 -0300

    refactoring Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,31 +4,29 @@ import { FiPlusSquare } from 'react-icons/fi';
 import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
 
-class Header extends Component {
-  render() {
-    const { openModal } = this.props;
+interface HeaderProps {
+  openModal: () => void;
+}
 
-    return (
-      <Container>
-        <header>
-          <img src={Logo} alt="GoRestaurant" />
-          <nav>
-            <div>
-              <button
-                type="button"
-                onClick={openModal}
-              >
-                <div className="text">Novo Prato</div>
-                <div className="icon">
-                  <FiPlusSquare size={24} />
-                </div>
-              </button>
-            </div>
-          </nav>
-        </header>
-      </Container>
-    )
-  }
-};
-
-export default Header;
+export default function Header({ openModal }: HeaderProps) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  )
+}
